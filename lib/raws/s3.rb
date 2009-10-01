138a7f25991bd44981ca9538e50869b220631bc8
Jun Kikuchi <jkikuchi@necosuke.jp>
2009-10-01 19:16:58 +0900

    S3 インタフェース取りかかり中...

--- lib/raws/s3.rb
+++ lib/raws/s3.rb
@@ -2,8 +2,20 @@ class RAWS::S3
   autoload :Adapter, 'raws/s3/adapter'
 
   class << self
+    def create_bucket(bucket_name)
+    end
+
+    def delete_bucket(bucket_name)
+    end
+
     def list
       Adapter.get_service
+    end
+
+    def each(&block)
+    end
+
+    def [](bucket_name)
     end
   end
 end
