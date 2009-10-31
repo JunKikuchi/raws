65a734141de2b3f585221c71fc4f6dce12ef0233
菊池 淳 <kikuchi@bonnou.com>
2009-10-31 16:05:29 +0900

    S3 のスペックを少し書いた

--- spec/raws/s3_spec.rb
+++ spec/raws/s3_spec.rb
@@ -1,27 +1,140 @@
 require 'spec/spec_config'
 
+#RAWS.logger.level = Logger::DEBUG
+
 describe RAWS::S3::Adapter do
+  before :all do
+=begin
+    RAWS_S3_BUCKETS.each do |name, location|
+      d RAWS::S3.create_bucket(name, location)
+    end
+    RAWS_S3_BUCKETS.each do |name, location|
+      RAWS::S3.put(name, 'a', 'A')
+      RAWS::S3.put(name, 'a/a', 'A-A')
+      RAWS::S3.put(name, 'a/a/a', 'A-A-A')
+      RAWS::S3.put(name, 'b', 'B')
+      RAWS::S3.put(name, 'c', 'C')
+    end
+=end
+  end
+
+  after :all do
+=begin
+    RAWS_S3_BUCKETS.each do |name, location|
+      RAWS::S3[name].delete_bucket
+    end
+=end
+  end
+
   describe 'class' do
-    it 'get_service' do
-      begin
-        p RAWS::S3::Adapter.get_service
-      rescue RAWS::Error => e
-        p e.response.code
-        p e.data
-      rescue => e
-        p e
+    it 'methods' do
+      %w'
+        create_bucket
+        delete_bucket
+        location
+        list
+        each
+        []
+        filter
+        put
+        copy
+        get
+        head
+        delete
+        acl
+      '.each do |val|
+        RAWS::S3.should respond_to val.to_sym
       end
     end
 
-    it 'put_bucket' do
+    it 'create_bucket' do
+    end
+
+    it 'delete_bucket' do
+    end
+
+    it 'location' do
       begin
-#        p RAWS::S3::Adapter.delete_bucket('kikuchitestbucket')
-#p RAWS::S3::Adapter.put_bucket('kikuchitestbucket')
-      rescue RAWS::Error => e
-        p e.response.code
-        p e.data
+        RAWS_S3_BUCKETS.each do |name, location|
+          RAWS::S3.location(name).should == (location || 'US')
+        end
       rescue => e
-        p e
+        d e.response.code
+        d e.response.header
+        d e.response.doc
+      end
+    end
+
+    it 'list' do
+      buckets = RAWS_S3_BUCKETS.map do |val|
+        val.first
+      end
+      RAWS::S3.list.each do |val|
+        buckets.should include(val['Name'])
+      end
+    end
+
+    it 'each' do
+      buckets = RAWS_S3_BUCKETS.map do |val|
+        val.first
+      end
+      RAWS::S3.each do |val|
+        buckets.should include(val['Name'])
+      end
+    end
+
+    it '[]' do
+      RAWS_S3_BUCKETS.each do |name, location|
+        RAWS::S3[name].should be_kind_of(RAWS::S3)
+        RAWS::S3[name].bucket_name.should == name
+      end
+    end
+
+    it 'filter' do
+      begin
+        RAWS_S3_BUCKETS.each do |name, location|
+          RAWS::S3.filter(name, 'prefix' => 'a/a/a')[0]['Key'].should == 'a/a/a'
+        end
+      rescue => e
+        d e.response.code
+        d e.response.header
+        d e.response.doc
+      end
+    end
+
+    it 'put' do
+    end
+
+    it 'copy' do
+    end
+
+    it 'get' do
+    end
+
+    it 'head' do
+    end
+
+    it 'delete' do
+    end
+
+    it 'acl' do
+    end
+  end
+
+  describe 'object' do
+    before do
+      @s3 = RAWS::S3[RAWS_S3_BUCKETS[0]]
+    end
+
+    it 'method' do
+      %w'
+        create_bucket
+        delete_bucket
+        location
+        filter
+        <=>
+      '.each do |val|
+        @s3.should respond_to val.to_sym
       end
     end
   end
