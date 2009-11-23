75c363fc489cef0dc9311a09f0fe486c788bbcbb
Jun Kikuchi <kikuchi@bonnou.com>
2009-11-23 21:21:51 +0900

    S3 の model 対応の spec ファイルを追加

---
+++ spec/raws/s3/model_spec.rb
@@ -0,0 +1,40 @@
+require 'spec/spec_config'
+
+class S3Object
+  include RAWS::S3::Model
+  self.bucket_name = RAWS_S3_BUCKETS[0][0]
+end
+
+describe RAWS::S3::Model do
+  describe 'class' do
+    it 'methods' do
+      %w'
+        create_bucket
+        delete_bucket
+        filter
+        all
+        find
+      '.each do |val|
+        S3Object.should respond_to val.to_sym
+      end
+    end
+  end
+
+  describe 'object' do
+    before do
+      @model = S3Object.all.first
+    end
+
+    it 'methods' do
+      %w'
+        header
+        metadata
+        send
+        receive
+      '.each do |val|
+        @model.should respond_to val.to_sym
+      end
+    end
+  end
+end
+
