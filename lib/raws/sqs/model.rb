74859a617476518526c8ff84a84b43390abe3a8a
Jun Kikuchi <kikuchi@bonnou.com>
2010-01-06 22:51:03 +0900

    RAWS::SQS::Model のクラスメソッドを queue への Forwardable とした。

--- lib/raws/sqs/model.rb
+++ lib/raws/sqs/model.rb
@@ -1,34 +1,32 @@
+require 'forwardable'
+
 module RAWS::SQS::Model
   module ClassMethods
+    extend Forwardable
+    def_delegators :queue,
+      :delete_queue,
+      :get_queue_attributes,
+      :set_queue_attributes,
+      :send_message,
+      :send,
+      :receive_message,
+      :receive,
+      :add_permission,
+      :remove_permission
+
     attr_accessor :queue_name
-
-    def create_queue
-      RAWS::SQS.create_queue(self.queue_name)
-    end
-
-    def delete_queue(force=nil)
-      RAWS::SQS.delete_queue(self.queue_name)
-    end
 
     def queue
       RAWS::SQS[self.queue_name]
     end
 
-    def send(model)
-      queue.send model
+    def create_queue
+      RAWS::SQS.create_queue(self.queue_name)
     end
-
-    def receive(params={}, *attrs)
-      queue.receive params, *attrs
-    end
-  end
-
-  module InstanceMethods
   end
 
   def self.included(mod)
     mod.class_eval do
-      include InstanceMethods
       extend ClassMethods
     end
   end
